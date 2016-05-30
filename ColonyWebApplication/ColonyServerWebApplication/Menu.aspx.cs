95b00cf2cb3dc72619afba500bb2a2642bb9a2f9
Administrator <Administrator@WIN-6I1FKE7RBR6>
2016-05-30 22:45:06 +0000

    update

--- ColonyWebApplication/ColonyServerWebApplication/Menu.aspx.cs
+++ ColonyWebApplication/ColonyServerWebApplication/Menu.aspx.cs
@@ -9,35 +9,48 @@ namespace ColonyServerWebApplication
 {
     public partial class Menu : System.Web.UI.Page
     {
+        MenuSupportClass supporter;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                supporter = new MenuSupportClass();
+            }
         }
 
         protected void ButtonWant_Click(object sender, EventArgs e)
         {
-
+            ButtonWant.Enabled = false;
+            Response.Redirect("WantTop.aspx");
+            ButtonWant.Enabled = true;
         }
 
         protected void ButtonGet_Click(object sender, EventArgs e)
         {
-
+            ButtonWant.Enabled = false;
+            Response.Redirect("GetTop.aspx");
+            ButtonWant.Enabled = true;
         }
 
- 
         protected void ButtonSearch_Click(object sender, EventArgs e)
         {
-
+            ButtonWant.Enabled = false;
+            Response.Redirect("SearchTop.aspx");
+            ButtonWant.Enabled = true;
         }
 
         protected void ButtonGive_Click(object sender, EventArgs e)
         {
-
+            ButtonWant.Enabled = false;
+            Response.Redirect("GiveTop.aspx");
+            ButtonWant.Enabled = true;
         }
 
         protected void ButtonConfig_Click(object sender, EventArgs e)
         {
-
+            ButtonWant.Enabled = false;
+            Response.Redirect("ConfigTop.aspx");
+            ButtonWant.Enabled = true;
         }
 
     }
